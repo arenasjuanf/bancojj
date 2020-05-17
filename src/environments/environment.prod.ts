d78340e70d771e7f2fbca36bd419fffba62f5a82
AlejoHurtatis14 <neideralejandro.h@gmail.com>
2020-05-17 13:53:31 -0500

    Se pone en firebase.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  urlApi: 'http://apparqueo.com/BancoMelo/public/api/'
+  urlApi: 'https://apparqueo.com/BancoMelo/public/api/',
+  firebaseConfig: {
+    apiKey: "AIzaSyA1u-NIwXf3WWyJ3XTiqccI4u1LXebQR1o",
+    authDomain: "bancowd-90580.firebaseapp.com",
+    databaseURL: "https://bancowd-90580.firebaseio.com",
+    projectId: "bancowd-90580",
+    storageBucket: "bancowd-90580.appspot.com",
+    messagingSenderId: "2520858988",
+    appId: "1:2520858988:web:0920cc47d4c5895602b349",
+    measurementId: "G-FM9X7FWBZ2"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,16 @@
 
 export const environment = {
   production: false,
-  urlApi: 'http://apparqueo.com/BancoMelo/public/api/'
+  urlApi: 'https://apparqueo.com/BancoMelo/public/api/',
   //urlApi: 'http://127.0.0.1:8000/api/'
+  firebaseConfig: {
+    apiKey: "AIzaSyA1u-NIwXf3WWyJ3XTiqccI4u1LXebQR1o",
+    authDomain: "bancowd-90580.firebaseapp.com",
+    databaseURL: "https://bancowd-90580.firebaseio.com",
+    projectId: "bancowd-90580",
+    storageBucket: "bancowd-90580.appspot.com",
+    messagingSenderId: "2520858988",
+    appId: "1:2520858988:web:0920cc47d4c5895602b349",
+    measurementId: "G-FM9X7FWBZ2"
+  }
 };
